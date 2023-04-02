374fd7eaf8a1467f48988dcfe8d9f5b91beeb0c1
omidespargham <omidespargham2001@gmail.com>
2023-04-02 20:33:08 +0330

    the get_category view make, with categorys serializer and urls

--- doctor/urls_v1.py
+++ doctor/urls_v1.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from doctor.views import FirstApi
+from doctor.views import FirstApi,GetCategorys
 
 app_name = "doctor_v1"
 
 urlpatterns = [
     path('first/',FirstApi.as_view(),name="first_api"),
+    path('get_categorys/',GetCategorys.as_view(),name="get_all_categorys"),
 ]
 
--- doctor/views.py
+++ doctor/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
+
+
 
 class FirstApi(APIView):
     def get(self,request):
         return Response({"this is make by avatar !"})
+    
+
+class GetCategorys(APIView):
+    def get(self,request):
+        categorys = Category.objects.all()
+        srz_categorys = CategorySerializer(instance=categorys,many=True)
+        return Response(data=srz_categorys.data)
