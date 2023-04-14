e02a08d780c077cf4f7f62a6e6f903ce5f83165e
omidespargham <omidespargham2001@gmail.com>
2023-04-14 20:01:27 +0330

    error for test api

--- doctor/urls_v1.py
+++ doctor/urls_v1.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from doctor.views import FirstApi,GetMainCategorysView
+from doctor.views import GetMainCategorysView
 
 app_name = "doctor_v1"
 
 urlpatterns = [
-    path('first/',FirstApi.as_view(),name="first_api"),
+    # path('first/',FirstApi.as_view(),name="first_api"),
     path('get_main_categorys/',GetMainCategorysView.as_view(),name="get_all_main_categorys"),
 ]
 
